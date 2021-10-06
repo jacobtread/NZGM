7c95f75e7d3b7495b02427eb94c519a55d8a0e86
Jacobtread <jacobtread@gmail.com>
2021-10-07 00:36:59 +1300

    Numeric Checker function

--- src/tools.ts
+++ src/tools.ts
@@ -2,26 +2,30 @@ import axios, { AxiosResponse } from "axios";
 import store from "./store";
 
 export function importFromCSV(file: string): void {
-    const lines: string[] = file.split("\n");
-    let header = true;
-    if (lines.length < 1) return;
-    store.state.rows = []; // Clear rows;
-    for (const line of lines) {
-        const parts: string[] = line.split(',');
-        if (header) {
-            store.state.cols = parts;
-            header = false;
-        } else {
-            store.state.rows.push(parts);
-        }
+  const lines: string[] = file.split("\n");
+  let header = true;
+  if (lines.length < 1) return;
+  store.state.rows = []; // Clear rows;
+  for (const line of lines) {
+    const parts: string[] = line.split(',');
+    if (header) {
+      store.state.cols = parts;
+      header = false;
+    } else {
+      store.state.rows.push(parts);
     }
+  }
 }
 export async function importCSVFromURL(url: string): Promise<void> {
-    try {
-        const response: AxiosResponse = await axios.get(url);
-        const data = response.data;
-        importFromCSV(data);
-    } catch (e) {
-        alert('Failed to load contents from ' + url)
-    }
+  try {
+    const response: AxiosResponse = await axios.get(url);
+    const data = response.data;
+    importFromCSV(data);
+  } catch (e) {
+    alert('Failed to load contents from ' + url)
+  }
+}
+
+export function isNumeric(/* eslint-disable */ value: any): boolean {
+  return value !== undefined && (typeof value === 'number' || /^-?\d+$/.test(value));
 }
