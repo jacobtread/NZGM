9fa6b6550555bdbc09be5205bfb8183931683ad0
jacobtread <jacobtread@users.noreply.github.com>
2021-10-06 06:31:11 +0000

    deploy: 636bbf20c897069d14fb50af541e1a301b254a06

---
+++ precache-manifest.9bc3f29aaf88d918deebb6d53e8df638.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984f9f6a021520530f90",
+    "url": "/NZGM/css/app.9d963ada.css"
+  },
+  {
+    "revision": "847a59ff1ddc2f24dedda39411456177",
+    "url": "/NZGM/img/logo-white.847a59ff.svg"
+  },
+  {
+    "revision": "a09664d4b26f2c2b4a9468833c97da8e",
+    "url": "/NZGM/index.html"
+  },
+  {
+    "revision": "984f9f6a021520530f90",
+    "url": "/NZGM/js/app.895c3781.js"
+  },
+  {
+    "revision": "4f368c676d9957670127",
+    "url": "/NZGM/js/chunk-vendors.826fc365.js"
+  },
+  {
+    "revision": "30c17263c3843ad500e3bd2ee6219165",
+    "url": "/NZGM/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/NZGM/robots.txt"
+  }
+]);
