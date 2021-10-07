6a4518889b8ae789a30e5c7becf141ad7d575ed1
Jacobtread <jacobtread@gmail.com>
2021-10-07 13:36:04 +1300

    Create example data function

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,21 @@ import "./registerServiceWorker";
 import store from "./store";
 import { importCSVFromURL } from "./tools";
 
-importCSVFromURL('https://raw.githubusercontent.com/mathsnz/NZGrapher/master/grapher/datasets/Babies.csv').then().catch();
+
+function importExample(): void {
+  importCSVFromURL('https://raw.githubusercontent.com/mathsnz/NZGrapher/master/grapher/datasets/Babies.csv').then().catch();
+  store.state.graph = {
+    title: 'Babies Graph',
+    type: 'dot-plot',
+    xAxis: 5,
+    yAxis: 4,
+    zAxis: -1,
+    size: 0,
+    scaleFactor: 1
+  }
+
+}
+
+importExample();
+
 createApp(App).use(store).mount("#app");
