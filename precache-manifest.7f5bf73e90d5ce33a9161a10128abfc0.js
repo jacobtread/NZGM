e6735d80b0172569809480194f211e4639079b54
jacobtread <jacobtread@users.noreply.github.com>
2021-10-09 05:06:58 +0000

    deploy: a0c40db476690355ae1daf49855bcafe3e3863b9

--- precache-manifest.7f5bf73e90d5ce33a9161a10128abfc0.js
+++ precache-manifest.7f5bf73e90d5ce33a9161a10128abfc0.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c6da6a32564ac3e3b154",
-    "url": "/NZGM/css/app.05fe2de8.css"
+    "revision": "2050579c3bdb12d18a1c",
+    "url": "/NZGM/css/app.18c1c12f.css"
   },
   {
     "revision": "847a59ff1ddc2f24dedda39411456177",
     "url": "/NZGM/img/logo-white.847a59ff.svg"
   },
   {
-    "revision": "40e78293110f1ba6defafff4cd666695",
+    "revision": "ae9f609214761d4d4c79f1ca928a63ae",
     "url": "/NZGM/index.html"
   },
   {
-    "revision": "c6da6a32564ac3e3b154",
-    "url": "/NZGM/js/app.2e212deb.js"
+    "revision": "2050579c3bdb12d18a1c",
+    "url": "/NZGM/js/app.915f2ded.js"
   },
   {
-    "revision": "3d696acf151b0371fd42",
-    "url": "/NZGM/js/chunk-vendors.074967f4.js"
+    "revision": "8912dfea895ede8d6fb8",
+    "url": "/NZGM/js/chunk-vendors.3e9b8ec7.js"
   },
   {
     "revision": "30c17263c3843ad500e3bd2ee6219165",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/NZGM/precache-manifest.a25eb73d8661d5c4931f6d9608f394d7.js"
+  "/NZGM/precache-manifest.7f5bf73e90d5ce33a9161a10128abfc0.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "nzgm"});
