69dc32344e6b75d5245924c0d13c8f6da82ea934
Jacobtread <jacobtread@gmail.com>
2021-10-11 18:27:43 +1300

    Moved Graph controls to its own component

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -11,156 +11,33 @@
     <div
       class="canvas-wrapper"
       id="canvasWrapper"
-      :class="{ scaled: graphValue.scaleFactor == 5 }"
+      :class="{ scaled: graph.scaleFactor == 5 }"
     >
       <img id="graph" src="" alt="" usemap="#canvas-map" />
       <canvas id="graphCanvas" usemap="#canvas-map" />
       <map name="canvas-map" id="canvasMap"></map>
     </div>
-    <div class="controls">
-      <div class="controls__box">
-        <div class="select">
-          <label class="select__label">X Axis</label>
-          <select class="select__input" name="" v-model="xAxis">
-            <option value="-1">None</option>
-            <option
-              :value="index"
-              v-for="(col, index) in data.cols"
-              :key="index"
-            >
-              {{ col }}
-            </option>
-          </select>
-        </div>
-        <div class="select">
-          <label class="select__label">Y Axis</label>
-          <select class="select__input" name="" v-model="yAxis">
-            <option value="-1">None</option>
-            <option
-              :value="index"
-              v-for="(col, index) in data.cols"
-              :key="index"
-            >
-              {{ col }}
-            </option>
-          </select>
-        </div>
-      </div>
-      <div class="controls__box">
-        <div class="select">
-          <label class="select__label">Z Axis</label>
-          <select class="select__input" name="" v-model="zAxis">
-            <option value="-1">None</option>
-            <option
-              :value="index"
-              v-for="(col, index) in data.cols"
-              :key="index"
-            >
-              {{ col }}
-            </option>
-          </select>
-        </div>
-        <div class="select">
-          <label class="select__label">Size</label>
-          <select
-            class="select__input"
-            name=""
-            v-model="size"
-            @change="resizeGraph"
-          >
-            <option value="0">Auto</option>
-            <option value="1">Auto - High Res</option>
-            <option value="2">Standard</option>
-            <option value="3">Small</option>
-            <option value="4">Short</option>
-          </select>
-        </div>
-      </div>
-      <div class="controls__box--long">
-        <label class="controls__box--long__title">Settings</label>
-        <div class="controls__box--long__content">
-          <template
-            v-for="(setting, index) in graphTypeData.settings"
-            :key="index"
-          >
-            <label class="checkbox" v-if="setting.type == 'toggle'">
-              <span class="checkbox__mark"></span>
-              <input
-                class="checkbox__input"
-                type="checkbox"
-                name=""
-                v-model="graphValue.settings.values[setting.key]"
-              />
-              <span class="checkbox__text">{{ setting.name }}</span>
-            </label>
-            <label class="slider" for="" v-if="setting.type == 'slider'">
-              <span class="slider__name"
-                >{{ setting.name }}
-                <span class="slider__value">{{
-                  graphValue.settings.values[setting.key]
-                }}</span></span
-              >
-              <span class="slider__wrapper">
-                <span class="slider__clamp">{{ setting.min }}</span>
-                <input
-                  class="slider__input"
-                  type="range"
-                  name=""
-                  id=""
-                  :min="setting.min"
-                  :max="setting.max"
-                  v-model="graphValue.settings.values[setting.key]"
-                />
-                <span class="slider__clamp">{{ setting.max }}</span>
-              </span>
-            </label>
-            <label class="input" for="" v-if="setting.type == 'number'">
-              <span class="input__name">{{ setting.name }} </span>
-              <input
-                type="number"
-                name=""
-                id=""
-                v-model="graphValue.settings.values[setting.key]"
-              />
-            </label>
-          </template>
-        </div>
-      </div>
-    </div>
+    <GraphControls @resize="resizeGraph" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import { mapState } from "vuex";
 
-import store, { GraphData, State } from "@/store";
+import store, { ContentData, GraphData } from "@/store";
 import { hideLoader, showLoader } from "@/tools";
 
-import { addDefaultSettings } from "@/graph";
-import { GraphTypeData } from "@/graph/types";
-import graphs from "@/graph/list";
 import { watermark } from "@/graph";
+import graphTypes from "@/graph/list";
+
+import GraphControls from "@/components/GraphControls.vue";
 
 @Options({
-  computed: mapState<State>({
-    data: (state: State) => state.data,
-    title: (state: State) => state.graph.title,
-  }),
+  components: {
+    GraphControls,
+  },
   watch: {
-    data: {
-      handler() {
-        this.renderGraph();
-      },
-      deep: true,
-    },
-    graph: {
-      handler() {
-        this.setupSettings();
-      },
-      deep: true,
-    },
-    graphValue: {
+    contentData: {
       handler() {
         this.renderGraph();
       },
@@ -174,20 +51,13 @@ export default class Graph extends Vue {
 
   mounted(): void {
     this.resizeGraph();
-    this.setupSettings();
-  }
-
-  setupSettings(): void {
-    const graphType = this.graphType;
-    const graph = graphs[graphType];
-    addDefaultSettings(graph.settings);
   }
 
   resizeGraph(): void {
     const graphWrapper: HTMLElement = document.getElementById(
       "canvasWrapper"
     ) as HTMLElement;
-    this.graphValue.scaleFactor = 1;
+    this.graph.scaleFactor = 1;
     if (this.size == 0) {
       // Auto
       this.width = graphWrapper.offsetWidth;
@@ -197,7 +67,7 @@ export default class Graph extends Vue {
       console.log(graphWrapper.offsetWidth, graphWrapper.offsetHeight);
       this.width = graphWrapper.offsetWidth * 5;
       this.height = graphWrapper.offsetHeight * 5;
-      this.graphValue.scaleFactor = 5;
+      this.graph.scaleFactor = 5;
     } else if (this.size == 2) {
       // Standard
       this.width = 800;
@@ -234,8 +104,7 @@ export default class Graph extends Vue {
     ctx.rect(0, 0, canvas.width, canvas.height);
     ctx.fill();
 
-    const graphType = this.graphType;
-    const graph = graphs[graphType];
+    const graph = graphTypes[this.graphType];
 
     graph.func(ctx);
 
@@ -250,10 +119,6 @@ export default class Graph extends Vue {
       "canvasMap"
     ) as HTMLMapElement;
     map.innerHTML = "";
-  }
-
-  get graphTypeData(): GraphTypeData {
-    return graphs[this.graphType];
   }
 
   download(): void {
@@ -287,39 +152,16 @@ export default class Graph extends Vue {
     return store.state.graph.size;
   }
 
-  set size(value: number) {
-    store.state.graph.size = value;
-  }
-
-  get xAxis(): number {
-    return store.state.graph.xAxis;
-  }
-
-  set xAxis(value: number) {
-    store.state.graph.xAxis = value;
-  }
-
-  get yAxis(): number {
-    return store.state.graph.yAxis;
-  }
-
-  set yAxis(value: number) {
-    store.state.graph.yAxis = value;
-  }
-  get zAxis(): number {
-    return store.state.graph.zAxis;
-  }
-
-  set zAxis(value: number) {
-    store.state.graph.zAxis = value;
-  }
-
   get graphType(): string {
     return store.state.graph.type;
   }
 
-  get graphValue(): GraphData {
+  get graph(): GraphData {
     return store.state.graph;
+  }
+
+  get contentData(): ContentData {
+    return store.state.data;
   }
 }
 </script>
@@ -352,43 +194,6 @@ export default class Graph extends Vue {
     background-color: $light-gray;
     border: none;
     cursor: pointer;
-  }
-}
-
-.controls {
-  flex: 0 0 auto;
-  display: flex;
-  flex-flow: row;
-  padding: 1em;
-  background-color: white;
-  position: relative;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.controls__box {
-  margin-right: 1em;
-
-  &--long {
-    flex: auto;
-    display: flex;
-    flex-flow: column;
-
-    &__title {
-      display: block;
-      padding: 0.5em 0;
-      text-align: left;
-      margin-left: 0.25em;
-    }
-
-    &__content {
-      overflow-y: auto;
-      border: 2px solid $light-gray;
-      padding: 0.5em;
-      display: flex;
-      flex-flow: column;
-      max-height: 7em;
-    }
   }
 }
 
---
+++ src/components/GraphControls.vue
@@ -0,0 +1,201 @@
+<template>
+  <div class="controls">
+    <div class="controls__box">
+      <div class="select">
+        <label class="select__label">X Axis</label>
+        <select class="select__input" name="" v-model="graphData.xAxis">
+          <option value="-1">None</option>
+          <option
+            :value="index"
+            v-for="(col, index) in contentData.cols"
+            :key="index"
+          >
+            {{ col }}
+          </option>
+        </select>
+      </div>
+      <div class="select">
+        <label class="select__label">Y Axis</label>
+        <select class="select__input" name="" v-model="graphData.yAxis">
+          <option value="-1">None</option>
+          <option
+            :value="index"
+            v-for="(col, index) in contentData.cols"
+            :key="index"
+          >
+            {{ col }}
+          </option>
+        </select>
+      </div>
+    </div>
+    <div class="controls__box">
+      <div class="select">
+        <label class="select__label">Z Axis</label>
+        <select class="select__input" name="" v-model="graphData.zAxis">
+          <option value="-1">None</option>
+          <option
+            :value="index"
+            v-for="(col, index) in contentData.cols"
+            :key="index"
+          >
+            {{ col }}
+          </option>
+        </select>
+      </div>
+      <div class="select">
+        <label class="select__label">Size</label>
+        <select
+          class="select__input"
+          name=""
+          v-model="graphData.size"
+          @change="this.$emit('resize')"
+        >
+          <option value="0">Auto</option>
+          <option value="1">Auto - High Res</option>
+          <option value="2">Standard</option>
+          <option value="3">Small</option>
+          <option value="4">Short</option>
+        </select>
+      </div>
+    </div>
+    <div class="controls__box--long">
+      <label class="controls__box--long__title">Settings</label>
+      <div class="controls__box--long__content">
+        <template v-for="(setting, index) in graphType.settings" :key="index">
+          <label class="checkbox" v-if="setting.type == 'toggle'">
+            <span class="checkbox__mark"></span>
+            <input
+              class="checkbox__input"
+              type="checkbox"
+              name=""
+              v-model="graphData.settings.values[setting.key]"
+            />
+            <span class="checkbox__text">{{ setting.name }}</span>
+          </label>
+          <label class="slider" for="" v-if="setting.type == 'slider'">
+            <span class="slider__name"
+              >{{ setting.name }}
+              <span class="slider__value">{{
+                graphData.settings.values[setting.key]
+              }}</span></span
+            >
+            <span class="slider__wrapper">
+              <span class="slider__clamp">{{ setting.min }}</span>
+              <input
+                class="slider__input"
+                type="range"
+                name=""
+                id=""
+                :min="setting.min"
+                :max="setting.max"
+                v-model="graphData.settings.values[setting.key]"
+              />
+              <span class="slider__clamp">{{ setting.max }}</span>
+            </span>
+          </label>
+          <label class="input" for="" v-if="setting.type == 'number'">
+            <span class="input__name">{{ setting.name }} </span>
+            <input
+              type="number"
+              name=""
+              id=""
+              v-model="graphData.settings.values[setting.key]"
+            />
+          </label>
+        </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import { addDefaultSettings } from "@/graph";
+import store, { ContentData, GraphData } from "@/store";
+import graphTypes from "@/graph/list";
+import { GraphTypeData } from "@/graph/types";
+
+@Options({
+  props: ["resizeGraph"],
+  watch: {
+    graphData: {
+      handler() {
+        this.setupSettings();
+      },
+      deep: true,
+    },
+  },
+})
+export default class GraphControls extends Vue {
+  width = 0;
+  height = 0;
+
+  mounted(): void {
+    this.setupSettings();
+  }
+
+  setupSettings(): void {
+    addDefaultSettings(this.graphType.settings);
+  }
+
+  get graphData(): GraphData {
+    return store.state.graph;
+  }
+
+  get graphType(): GraphTypeData {
+    return graphTypes[this.graphData.type];
+  }
+
+  get size(): number {
+    return store.state.graph.size;
+  }
+
+  set size(value: number) {
+    store.state.graph.size = value;
+  }
+
+  get contentData(): ContentData {
+    return store.state.data;
+  }
+}
+</script>
+<style lang="scss" scoped>
+@import "@/assets/scss/variables.scss";
+
+.controls {
+  flex: 0 0 auto;
+  display: flex;
+  flex-flow: row;
+  padding: 1em;
+  background-color: white;
+  position: relative;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.controls__box {
+  margin-right: 1em;
+
+  &--long {
+    flex: auto;
+    display: flex;
+    flex-flow: column;
+
+    &__title {
+      display: block;
+      padding: 0.5em 0;
+      text-align: left;
+      margin-left: 0.25em;
+    }
+
+    &__content {
+      overflow-y: auto;
+      border: 2px solid $light-gray;
+      padding: 0.5em;
+      display: flex;
+      flex-flow: column;
+      max-height: 7em;
+    }
+  }
+}
+</style>
