00909cd8c48506a9b7669a7862095ff4b5c0f6eb
Jacobtread <jacobtread@gmail.com>
2021-10-13 00:54:17 +1300

    Updated vue config for correct usage of pwa

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
-    publicPath: process.env.GITHUB_PAGES
-      ? "/NZGM/"
-      : "/"
+  publicPath: process.env.GITHUB_PAGES
+    ? "/NZGM/"
+    : "/",
+  pwa: {
+    name: "NZGM",
+    themeColor: "#1565c0",
+    assetsVersion: "0.1.0",
+    iconPaths: {
+      faviconSVG: 'img/icons/logo.svg',
+      favicon32: 'img/icons/favicon-32x32.png',
+      favicon16: 'img/icons/favicon-16x16.png',
+      appleTouchIcon: 'img/icons/apple-touch-icon.png',
+      maskIcon: 'img/icons/logo.svg',
+      msTileImage: null
+    }
   }
+}
